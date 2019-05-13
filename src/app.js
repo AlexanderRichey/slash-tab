36d3d7d8845bc92ae91dea2a26797dcc9728b79b
AlexanderRichey <alexander.richey@gmail.com>
2019-05-13 11:12:49 -0700

    add sentry, clean up unused deps

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 
 import App from "./components/App";
+
+Sentry.init({
+  dsn: "https://96f55e2f37b6489a8d2208ed35dd1b22@sentry.io/1458506"
+});
+
+window.addEventListener("error", e => Sentry.captureException(e));
 
 const root = document.getElementById("root");
 ReactDOM.render(<App />, root);
--- src/popup.js
+++ src/popup.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom";
 
 import Popup from "./components/Popup";
 
+Sentry.init({
+  dsn: "https://96f55e2f37b6489a8d2208ed35dd1b22@sentry.io/1458506"
+});
+
+window.addEventListener("error", e => Sentry.captureException(e));
+
 const root = document.getElementById("root");
 ReactDOM.render(<Popup />, root);
